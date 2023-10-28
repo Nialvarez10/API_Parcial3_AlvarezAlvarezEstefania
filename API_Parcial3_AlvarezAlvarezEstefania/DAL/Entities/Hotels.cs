0aa44e78bbae77995febaef28b0097cef5860b3d
Estefania <nialvarez10@gmail.com>
2023-10-28 15:50:25 -0500

    Entities Hotels and Rooms Created

---
+++ API_Parcial3_AlvarezAlvarezEstefania/DAL/Entities/Hotels.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Parcial3_AlvarezAlvarezEstefania.DAL.Entities
+{
+    public class Hotels : AuditBase
+    {
+        [Display(Name = "Name")]
+        [MaxLength(50, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
+        [Required(ErrorMessage = "¡The field {0} is required!")]
+        public string Name { get; set; }
+
+    
+        [Required(ErrorMessage = "¡The field {0} is required!")]
+        public string Address { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Reputation must be in the range of 1 to 5 stars.")]
+        public int Stars { get; set; }
+
+        public ICollection<Rooms>? Room { get; set; }
+    }
+}
---
+++ API_Parcial3_AlvarezAlvarezEstefania/DAL/Entities/Rooms.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Parcial3_AlvarezAlvarezEstefania.DAL.Entities
+{
+    public class Rooms : AuditBase
+    {
+        [Display(Name = "Number")]
+        [MaxLength(3, ErrorMessage = "The field {0} must have a maximum of {1} characters")]
+        [Required(ErrorMessage = "¡The field {0} is required!")]
+        public string Number { get; set; }
+        public int MaxGuests { get; set; }
+
+        [Required(ErrorMessage = "¡The field {0} is required!")]
+        public bool Availability { get; set; }
+        public Guid? HotelId { get; set; } //Fk
+        public Hotels? Hotel { get; set; }
+    }
+}
