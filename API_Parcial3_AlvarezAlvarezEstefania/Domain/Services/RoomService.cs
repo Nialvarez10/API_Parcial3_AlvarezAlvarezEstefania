7237a7c87d7f3787f699fe85ab18e9066652bbb7
Estefania <nialvarez10@gmail.com>
2023-10-28 18:04:45 -0500

    Modified  Program and appsettings

--- API_Parcial3_AlvarezAlvarezEstefania/Domain/Services/RoomService.cs
+++ API_Parcial3_AlvarezAlvarezEstefania/Domain/Services/RoomService.cs
@@ -1,6 +1,26 @@
-﻿namespace API_Parcial3_AlvarezAlvarezEstefania.Domain.Services
+﻿using API_Parcial3_AlvarezAlvarezEstefania.DAL.Entities;
+using API_Parcial3_AlvarezAlvarezEstefania.DAL;
+using API_Parcial3_AlvarezAlvarezEstefania.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Parcial3_AlvarezAlvarezEstefania.Domain.Services
 {
-    public class RoomService
+    public class RoomService : IRoomService
     {
+        private readonly DataBaseContext _context;
+
+        public RoomService(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Room> GetRoomByNumberWithAvailabilityAsync(Guid hotelId, string roomNumber)
+        {
+            var room = await _context.Rooms
+                .Where(r => r.HotelId == hotelId && r.Number == roomNumber && r.Availability)
+                .FirstOrDefaultAsync();
+
+            return room;
+        }
     }
 }
