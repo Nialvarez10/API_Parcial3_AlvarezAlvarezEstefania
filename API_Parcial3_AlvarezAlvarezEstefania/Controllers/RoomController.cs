27140032ded2a6b80c80899589f917e658ccb202
Estefania <nialvarez10@gmail.com>
2023-10-28 23:33:58 -0500

    Changes made today

--- API_Parcial3_AlvarezAlvarezEstefania/Controllers/RoomController.cs
+++ API_Parcial3_AlvarezAlvarezEstefania/Controllers/RoomController.cs
@@ -25,7 +25,7 @@ namespace API_Parcial3_AlvarezAlvarezEstefania.Controllers
             if (room == null)
             {
                 // Room is not available, displays an error message.
-                return NotFound($"Room {roomNumber} of the hotel [Hotel.name] already booked");
+                return BadRequest($"Room {roomNumber} of the hotel [Hotel.name] already booked");
             }
 
             // The room is available, return the entire room item.
